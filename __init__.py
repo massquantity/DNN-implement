d752ee9d37fd904c27761fe81d5fb8f235aa9cf2
massquantity <wdmjjxg@163.com>
2019-03-15 16:56:46 +0800

    第一次

---
+++ __init__.py
@@ -0,0 +1,7 @@
+from .data import cifar_data
+from .model import Network
+from .train import train
+from .evaluate import evaluate
+from .utils import optimizers, activations
+from .utils.optimizers import *
+from .utils.activations import *
