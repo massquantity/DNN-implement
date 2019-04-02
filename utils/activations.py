a671b7fe501b976a930bcdb35ad5ca16fe5711dc
massquantity <wdmjjxg@163.com>
2019-04-02 10:19:27 +0800

    activations

--- utils/activations.py
+++ utils/activations.py
@@ -2,15 +2,15 @@ import numpy as np
 
 
 class Sigmoid:
-    def forward(self, z):
+    def __call__(self, z):
         return 1.0 / (1.0 + np.exp(-z))
 
     def derivative(self, z):
-        return self.forward(z) * (1 - self.forward(z))
+        return self.__call__(z) * (1 - self.__call__(z))
 
 
 class Tanh:
-    def forward(self, z):
+    def __call__(self, z):
         return np.tanh(z)
 
     def derivative(self, z):
@@ -18,7 +18,7 @@ class Tanh:
 
 
 class ReLU:
-    def forward(self, z):
+    def __call__(self, z):
         return np.maximum(z, 0)
 
     def derivative(self, z):
@@ -32,8 +32,7 @@ class LeakyReLU:
     def __init__(self, alpha=0.3):
         self.alpha = alpha
 
-        print(alpha)
-    def forward(self, z):
+    def __call__(self, z):
         return np.where(z >= 0, z, self.alpha * z)
 
     def derivative(self, z):
@@ -44,7 +43,7 @@ class ELU:
     def __init__(self, alpha=1.0):
         self.alpha = alpha
 
-    def forward(self, z):
+    def __call__(self, z):
         return np.where(z >= 0, z, self.alpha * (np.exp(z) - 1))
 
     def derivative(self, z):
@@ -56,7 +55,7 @@ class Selu:
         self.alpha = 1.6732632423543772848170429916717
         self.scale = 1.0507009873554804934193349852946
 
-    def forward(self, z):
+    def __call__(self, z):
         return self.scale * np.where(z >= 0, z, self.alpha * (np.exp(z) - 1))
 
     def derivative(self, z):
@@ -88,37 +87,4 @@ class Softmax:
         return a - b
 
 
-
-
-
-
-'''
-def sigmoid(z):
-    return 1.0 / (1.0 + np.exp(-z))
-
-def sigmoid_derivative(z):
-    return sigmoid(z) * (1 - sigmoid(z))
-
-def relu(z):
-    return np.maximum(z, 0)
-
-def relu_derivative(z):
-    mask = (z <= 0)
-    dout = np.ones(z.shape)
-    dout[mask] = 0.0
-    return dout
-
-def softmax(z):
-    z = z - np.max(z)
-    return np.exp(z) / np.sum(np.exp(z))
-
-def softmax_batch(z):
-    z = z.T
-    z = z - np.max(z, axis=0)
-    t = np.exp(z) / np.sum(np.exp(z), axis=0)
-    return t.T
-
-def softmax_derivative(a, b):
-    return a - b
-'''
 
