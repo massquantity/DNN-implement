4be319d981d36e0d3781a1ea745612d934f927ce
massquantity <wdmjjxg@163.com>
2019-04-24 16:28:35 +0800

    test

--- tests/train_network.py
+++ tests/train_network.py
@@ -8,34 +8,31 @@ from DNN_implementation import Sigmoid, ReLU
 
 if __name__ == "__main__":
     print("------------------", "test benchmark", "----------------------")
-    '''
+
     (X_train, y_train), (X_test, y_test) = cifar_data.load_data(normalize=False, standard=True)  # standardscale
-    dnn = Network_mini_batch(sizes=[3072, 50, 10], activation="selu", alpha=0.01, dropout_rate=0.5,
-                             weight_initializer="xavier")
-    optimizer = Momentum(lr=1e-3, momentum=0.9, batch_size=128)
-    train_DNN_minibatch(X_train, y_train, 100, optimizer, 128, dnn, X_test, y_test,
-                        early_stopping=False, tolerance=0.0, metrics="accuracy")  # lr_decay_rate=0.95
-    print()
-    '''
-    '''
-    (X_train, y_train), (X_test, y_test) = cifar_data.load_data(normalize=False, standard=True)  # standardscale
-    dnn = Network_mini_batch(sizes=[3072, 50, 10], activation="relu")
-    optimizer = Momentum(lr=1e-3, momentum=0.9, batch_size=128)
-    train_DNN_minibatch(X_train, y_train, 100, optimizer, 128, dnn, X_test, y_test,
-                        early_stopping=True, patience=20,  tolerance=0.0, metrics="accuracy")
-    print()
-    '''
-    (X_train, y_train), (X_test, y_test) = mnist_2.load_data()
-    dnn = Network_mini_batch(sizes=[784, 300, 200, 10], activation="selu", dropout_rate=0.0,  # dropout alpha
-                             weight_initializer="he")
-    optimizer = Adam(lr=1e-3, batch_size=64)
-#    optimizer = Sgd(lr=1e-3, batch_size=64)
-    start_time = time.time()
-    train_DNN_minibatch(X_train, y_train, 150, optimizer, 64, dnn, X_test, y_test,
-                        early_stopping=True, patience=20, metrics="accuracy", evaluate=True)  # lr_decay_rate=0.95
-    print("training time: {:.2f}".format(time.time() - start_time))
+    dnn = Network_mini_batch(sizes=[3072, 1000, 500, 200, 10], activation="sigmoid", alpha=0.01, dropout_rate=0.0,
+                             weight_initializer="normal")
+    optimizer = Sgd(lr=0.7, batch_size=512)
+#    optimizer = Momentum(lr=1e-3, momentum=0.9, batch_size=256)
+#    optimizer = Adam(lr=1e-3, batch_size=256)
+    train_DNN_minibatch(X_train, y_train, 150, optimizer, 512, dnn, X_test, y_test,
+                        early_stopping=True, patience=20, metrics="accuracy", evaluate=True,
+                        )  # lr_decay_rate=0.99, lr_decay_mode="exponential"
     print()
 
+    '''
+    (X_train, y_train), (X_test, y_test) = mnist_2.load_data()
+    dnn = Network_mini_batch(sizes=[784, 1000, 500, 200, 10], activation="leaky_relu", alpha=0.1, dropout_rate=0.0,  # dropout alpha
+                             weight_initializer="he")
+    optimizer = Adam(lr=1e-3, batch_size=256)  # 1e-3
+#    optimizer = Sgd(lr=1e-3, batch_size=64)
+    start_time = time.time()
+    train_DNN_minibatch(X_train, y_train, 150, optimizer, 256, dnn, X_test, y_test,
+                        early_stopping=True, patience=20, metrics="accuracy", evaluate=True,
+                        lr_decay_rate=0.99, lr_decay_mode="exponential")
+    print("training time: {:.2f}".format(time.time() - start_time))
+    print()
+    '''
 
 
 
